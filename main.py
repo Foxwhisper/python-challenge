2f9b1e811b5a5b73b9aa02cfc3a206d670cf1281
Heidi Fox <heidi_fox831@yahoo.com>
2024-07-30 19:13:26 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 """
-#Your task is to create a Python script that analyzes the records to calculate each of the following values:
-#The total number of months included in the dataset
-#The net total amount of "Profit/Losses" over the entire period
-#The changes in "Profit/Losses" over the entire period, and then the average of those changes
-#The greatest increase in profits (date and amount) over the entire period
-#The greatest decrease in profits (date and amount) over the entire period
-#print the analysis to the terminal and export a text file with the results
+In this Challenge, you are tasked with helping a small, rural town modernize its vote-counting process.
+
+You will be given a set of poll data called election_data.csv. The dataset is composed of three columns: "Voter ID", "County", and "Candidate". Your task is to create a Python script that analyzes the votes and calculates each of the following values:
+
+The total number of votes cast
+
+A complete list of candidates who received votes
+
+The percentage of votes each candidate won
+
+The total number of votes each candidate won
+
+The winner of the election based on popular vote
 """
 
 #import files to use in this project
@@ -13,72 +19,60 @@ import os
 import csv
 
 #path for csv file and text file
-budget_data_csv = os.path.join(".","Resources", "budget_data.csv")
-analysis_txt = os.path.join(".", "Analysis", "budget_analysis.txt")
+election_data_csv = os.path.join(".","Resources", "election_data.csv")
+analysis_txt = os.path.join(".", "Analysis", "election_analysis.txt")
 
-#declare variables
-month = 0
-grand_total = 0
-previous_value = 0
-changes = []
-average_change = 0
-total_change = 0
-low_change = 0
-high_change = 0
-low_month = ["",0]
-high_month = ["",0]
-
-
+#variables
+candidates = []
+votes = []
 
 #open the cvs file
-with open(budget_data_csv, 'r') as file:
+with open(election_data_csv, 'r') as file:
     csvreader=csv.reader(file,delimiter=",")
+    
     header = next(csvreader)
     
-    #Loops
-
     for row in csvreader:
-        current_value = int(row[1])
-        #how to count the amount of months
-        month += 1
-        grand_total += current_value
+        
+        if row[2] in candidates:
+        	#count up 1
+            index = candidates.index(row[2])
+            votes[index] += 1
+#is this the beginning 
+        else:
+        	index = len(candidates)
+        	candidates.append(row[2])
+        	votes.append(1)
+
+#determine the winner
+winner_index = votes.index(max(votes))
 
 
-#profit/losses
-        if previous_value != 0:
-            change = current_value - previous_value
-            changes.append(change)
+print('Election Results\n')
+print('--------------------------\n')
+print(f'Total Votes: {sum(votes)}\n')
+print('--------------------------\n'
+	)
 
-            if change > high_change:
-                high_change = change
-            if change > high_month[1]:
-                high_month[0] = row[0]
-                high_month[1] = change
+#votes received by person and the percent of votes each candidate won
+#and the winner
+#print the results
+for x in range(0,len(candidates)):
+	print(f'{candidates[x]}: {round(votes[x] / sum(votes) * 100,3)}% ({votes[x]})')
+print('--------------------------\n')
+print(f'Winner: {candidates[winner_index]}\n')
 
-            elif change < low_change:
-                low_change = change
-            elif change < low_month[1]:
-                low_month[0] = row[0]
-                low_month[1] = change
-
-        previous_value = current_value
-
-
-#The changes in "Profit/Losses" over the entire period, and then the average of those changes
-    average_change = sum(changes) / len(changes)
-
-
-# create the output
-print('Financial Analysis\n', '\r')
-print('--------------------------\n', '\r'
-        f'Total Month: {month}\n'
-        f'Total: ${grand_total}\n'
-        f'Average Change: ${round(average_change,2)}\n')
-print(f'Greatest Increase in Profits: {high_month[0]} (${high_month[1]})\n'
-    f'Greatest Decrease in Profits: {low_month[0]} (${low_month[1]})\n')
-
-
-
-
-
-
+with open(analysis_txt, 'w', newline='') as output:
+	output.write(
+		'Election Results\n'
+		'--------------------\n'
+		f'Total Votes: {sum(votes)}\n'
+		'--------------------\n'
+	)
+	for x in range(0,len(candidates)):
+		output.write(f'{candidates[x]}: {round(votes[x] / sum(votes) * 100,2)}% ({votes[x]})\n')
+	output.write(
+		'--------------------\n'
+		f'Winner: {candidates[winner_index]}\n'
+		'--------------------\n'
+	)	
